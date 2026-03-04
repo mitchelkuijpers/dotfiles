e0227b79c1b0d2f6020f4fc3505f57809408bd3c
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2026-03-04 19:29:16 +0000

    Wrap pi in fish with isolated npm global prefix

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -6,6 +6,13 @@ _: {
 
       # Keep Nix profile paths in front so Nix-installed tools win over system ones.
       fish_add_path --prepend --move ~/.nix-profile/bin /etc/profiles/per-user/$USER/bin
+
+      # pi wrapper: isolate npm global installs to a writable user directory.
+      function pi --wraps pi --description "Run pi with isolated npm global prefix"
+        set -l pi_npm_prefix "$HOME/.pi/npm-global"
+        mkdir -p "$pi_npm_prefix"
+        env NPM_CONFIG_PREFIX="$pi_npm_prefix" command pi $argv
+      end
     '';
   };
 }
