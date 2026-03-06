d9b8fd742236100d0f0f8b2ea91bc44c867a2688
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2026-03-06 09:26:52 +0100

    Upgrade pi-coding-agent to 0.56.2

--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -21,7 +21,7 @@
       npmDeps = pkgs.importNpmLock {
         inherit package packageLock;
       };
-      npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+      inherit (pkgs.importNpmLock) npmConfigHook;
 
       # Keep builds deterministic and avoid lifecycle scripts reaching out to the network.
       npmInstallFlags = ["--ignore-scripts"];
@@ -30,9 +30,9 @@
 
   piCodingAgent = mkPinnedNpmCli {
     pname = "pi-coding-agent";
-    version = "0.55.4";
-    url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-0.55.4.tgz";
-    hash = "sha256-GnzPe1QJcFhpLZVlREvgRu/zh3GTXxeK/o58DdQ6QiI=";
+    version = "0.56.2";
+    url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-0.56.2.tgz";
+    hash = "sha256-XmokVNrL1Fm+zffFUpJ+a05Oq7iGLYV3HLe5OE9jNVk=";
     packageFile = ../../assets/npm-locks/pi-coding-agent.package.json;
     lockFile = ../../assets/npm-locks/pi-coding-agent.package-lock.json;
   };
