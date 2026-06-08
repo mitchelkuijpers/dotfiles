7cba39df9b3ffb3cb7e3725e1896a69da76275c2
Mitchel Kuijpers <mitchel.kuijpers@avisi.nl>
2026-06-08 11:34:06 +0200

    chore: use XDG dataHome for pnpm and add to session path

--- modules/common/env.nix
+++ modules/common/env.nix
@@ -1,14 +1,17 @@
-_: {
+{config, ...}: let
+  pnpmHome = "${config.xdg.dataHome}/.local/share/pnpm";
+in {
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
     LANG = "en_US.UTF-8";
     CONNECT__BASEURL = "https://mitchel.eu.ngrok.io";
-    PNPM_HOME = "$HOME/.local/share/pnpm";
+    PNPM_HOME = pnpmHome;
   };
 
   home.sessionPath = [
     "/opt/homebrew/bin"
     "/opt/homebrew/sbin"
+    pnpmHome
   ];
 }
