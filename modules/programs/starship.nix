f14e036f2ba9e927350d82dde438d595c811fdef
Mitchel Kuijpers <mitchel.kuijpers@avisi.nl>
2026-06-29 21:40:02 +0200

    feat: enable mise in starship prompt

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -2,5 +2,17 @@ _: {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+
+      mise.disabled = false;
+    };
   };
 }
